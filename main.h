7a0ef8a43099ee6073288c6e44c39d2d5de2f406
Natalia Heredia <91100868+nataliazzz@users.noreply.github.com>
2023-03-30 15:53:07 -0600

    1

--- main.h
+++ main.h
@@ -12,7 +12,11 @@
 * Description: struct that stores pointers to a
 * printer functions.
 */
-
+typedef struct print
+{
+  char *type_arg;
+  int (*f)(va_list, char *, unsigned int);
+} print_t;
 int _printf(const char *format, ...);
 int print_prg(va_list __attribute__((unused)), char *, unsigned int);
 int print_chr(va_list arguments, char *buf, unsigned int ibuf);
@@ -22,11 +26,4 @@ int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int print_buf(char *buf, unsigned int nbuf);
-
-typedef struct print
-{
-  char *type_arg;
-  int (*f)(va_list, char *, unsigned int);
-} print_t;
-
 #endif
