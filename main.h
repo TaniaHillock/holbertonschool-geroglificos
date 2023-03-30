25035035532f71e48efba9be6826d978e0c641fe
Natalia Heredia <91100868+nataliazzz@users.noreply.github.com>
2023-03-30 15:33:48 -0600

    fix

--- main.h
+++ main.h
@@ -27,4 +27,5 @@ int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int print_buf(char *buf, unsigned int nbuf);
+
 #endif
