8c1d0dbce11fa7e4ce0bff5cfc8866956fc41576
Natalia Heredia <91100868+nataliazzz@users.noreply.github.com>
2023-03-30 15:32:36 -0600

    fix

--- print_chr.c
+++ print_chr.c
@@ -1,17 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_char - print char
- * @list: va_list.
- *
- * Return: 1
- */
-int print_char(va_list list)
+* print_chr - writes the character c to stdout
+* @arguments: input char
+* @buf: buffer pointer
+* @ibuf: index for buffer pointer
+* Return: On success 1.
+*/
+int print_chr(va_list arguments, char *buf, unsigned int ibuf)
 {
-	int aux;
 
-	aux = va_arg(list, int);
-
-	_putchar(aux);
-
+	char c;
+	
+	c = va_arg(arguments, int);
+	handl_buf(buf, c, ibuf);
+	
 	return (1);
+	
 }
