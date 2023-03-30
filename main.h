2366d279862cfdecdc782a7398513a9fe09d397a
Natalia Heredia <91100868+nataliazzz@users.noreply.github.com>
2023-03-30 15:28:02 -0600

    fix

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
+
 /**
 * struct print - struct for printer functions
 * @type_arg: identifier
@@ -10,11 +11,13 @@
 * Description: struct that stores pointers to a
 * printer functions.
 */
+
 typedef struct print
 {
 char *type_arg;
 int (*f)(va_list, char *, unsigned int);
 } print_t;
+
 int _printf(const char *format, ...);
 int print_prg(va_list __attribute__((unused)), char *, unsigned int);
 int print_chr(va_list arguments, char *buf, unsigned int ibuf);
@@ -24,4 +27,5 @@ int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int print_buf(char *buf, unsigned int nbuf);
+
 #endif
