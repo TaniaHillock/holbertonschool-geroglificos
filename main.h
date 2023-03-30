2f99eb3bf43b3e09bea2eb980fe54d0af5d21385
Natalia Heredia <91100868+nataliazzz@users.noreply.github.com>
2023-03-30 15:27:00 -0600

    fix

--- main.h
+++ main.h
@@ -1,50 +1,27 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdarg.h>
-
-/* utils.c */
-int _strlen(const char *);
-int print(char *);
-char *itoa(long int, int);
-
-/* printf.c */
-int _printf(const char *, ...);
-
-/* handler.c */
-int handler(const char *, va_list);
-int percent_handler(const char *, va_list, int *);
-
-/* printers */
-int print_string(va_list);
-int print_char(va_list);
-int print_integer(va_list);
-int print_binary(va_list);
-int print_rot(va_list);
-int print_unsigned(va_list);
-int print_octal(va_list);
-int print_hexadecimal_low(va_list);
-int print_hexadecimal_upp(va_list);
-int print_pointer(va_list);
-int print_rev_string(va_list);
-
-/* _putchar.c */
-int _putchar(char);
-int buffer(char);
-
 /**
- * struct _format - Typedef struct
- *
- * @type: Format
- * @f: The function associated
- **/
-typedef struct _format
+* struct print - struct for printer functions
+* @type_arg: identifier
+* @f: pointer to a printer functions
+* Description: struct that stores pointers to a
+* printer functions.
+*/
+typedef struct print
 {
-	char type;
-	int (*f)(va_list);
-} format;
-
-
+char *type_arg;
+int (*f)(va_list, char *, unsigned int);
+} print_t;
+int _printf(const char *format, ...);
+int print_prg(va_list __attribute__((unused)), char *, unsigned int);
+int print_chr(va_list arguments, char *buf, unsigned int ibuf);
+int print_str(va_list arguments, char *buf, unsigned int ibuf);
+int print_int(va_list arguments, char *buf, unsigned int ibuf);
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+int ev_print_func(const char *s, int index);
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
+int print_buf(char *buf, unsigned int nbuf);
 #endif
