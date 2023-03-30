116216ebf9bf19e2891391c8666b1b2fba5c4263
Natalia Heredia <91100868+nataliazzz@users.noreply.github.com>
2023-03-30 15:46:39 -0600

    1

--- main.h
+++ main.h
@@ -17,6 +17,7 @@ typedef struct print
   char *type_arg;
   int (*f)(va_list, char *, unsigned int);
 } print_t;
+
 int _printf(const char *format, ...);
 int print_prg(va_list __attribute__((unused)), char *, unsigned int);
 int print_chr(va_list arguments, char *buf, unsigned int ibuf);
