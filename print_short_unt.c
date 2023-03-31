611d7e73d299539b8d20c9207e69ed43090503c4
taniahillock <tania.hillock@gmail.com>
2023-03-31 10:51:21 -0600

    fixes

--- print_short_unt.c
+++ print_short_unt.c
@@ -1,48 +1,29 @@
 #include "main.h"
 /**
- * prinhupx - prints a short decimal in hexadecimal
- * @arguments: The character to print
+ * prinhunt - prints a short unsigned integer
+ * @arguments: number to print
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
- * Return: number of chars printed
+ * Return: number of chars printed.
  */
-int prinhupx(va_list arguments, char *buf, unsigned int ibuf)
+int prinhunt(va_list arguments, char *buf, unsigned int ibuf)
 {
-	short int int_input, i, isnegative, count, first_digit;
-	char *hexadecimal, *binary;
+	unsigned short int int_in, int_temp, i, div;
 
-	int_input = va_arg(arguments, int);
-	isnegative = 0;
+	int_in = va_arg(arguments, unsigned int);
 
-	if (int_input == 0)
+	int_temp = int_in;
+	div = 1;
+
+	while (int_temp > 9)
 	{
-		ibuf = handl_buf(buf, '0', ibuf);
-		return (1);
-	}
-	if (int_input < 0)
-	{
-		int_input = (int_input * -1) - 1;
-		isnegative = 1;
+		div *= 10;
+		int_temp /= 10;
 	}
 
-	binary = malloc(sizeof(char) * (16 + 1));
-	binary = fill_binary_array(binary, int_input, isnegative, 16);
-	hexadecimal = malloc(sizeof(char) * (4 + 1));
-	hexadecimal = fill_hex_array(binary, hexadecimal, 1, 4);
-
-	for (first_digit = i = count = 0; hexadecimal[i]; i++)
+	for (i = 0; div > 0; div /= 10, i++)
 	{
-		if (hexadecimal[i] != '0' && first_digit == 0)
-			first_digit = 1;
-		if (first_digit)
-		{
-			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
-			count++;
-		}
+		ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
 	}
-
-	free(binary);
-	free(hexadecimal);
-
-	return (count);
+	return (i);
 }
